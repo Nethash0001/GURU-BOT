bb539e7bde0dc03bf0ecca8acf0b560f340b0e42
Error#0001 <89148628+Nethash0001@users.noreply.github.com>
2023-11-17 00:01:50 +0400

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['971569671530', 'D-Evil Bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
